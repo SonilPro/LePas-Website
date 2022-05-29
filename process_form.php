465767fd2cdf27f6e2e137c4d640d2353a2682fc
Antonio Ereiz <51959110+SonilPro@users.noreply.github.com>
2022-05-29 21:14:28 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -84,10 +84,10 @@ if (isset($_FILES['images']['name'])) {
     $mail->SMTPAuth = true;
 
     //Username to use for SMTP authentication - use full email address for gmail
-    $mail->Username = 'd5b3a2d3ec1b7e';
+    $mail->Username = '';
 
     //Password to use for SMTP authentication
-    $mail->Password = 'e1a97c83c65dff';
+    $mail->Password = '';
 
     //Set who the message is to be sent from
     //Note that with gmail you can only use your account address (same as `Username`)
